e66c97039c671bfe5752ace00101a41f91375611
pareekvinit12-ai <pareekvinit12@gmail.com>
2026-03-20 19:01:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let main = document.querySelector(".main");
+
 let colors = [
   "red", "red",
   "blue", "blue",
@@ -7,6 +8,7 @@ let colors = [
   "orange", "orange",
   "purple", "purple"
 ];
+
 colors.sort(() => Math.random() - 0.5);
 
 let first = null;
@@ -18,10 +20,27 @@ for (let i = 0; i < 12; i++) {
   box.className = "box";
   box.dataset.color = colors[i];
 
+  let inner = document.createElement("div");
+  inner.className = "inner";
+
+  let front = document.createElement("div");
+  front.className = "front";
+
+  let back = document.createElement("div");
+  back.className = "back";
+
+  inner.appendChild(front);
+  inner.appendChild(back);
+  box.appendChild(inner);
+
   box.addEventListener("click", function () {
     if (lock || box.classList.contains("open")) return;
-    box.style.background = box.dataset.color;
+
     box.classList.add("open");
+
+    setTimeout(() => {
+      back.style.background = box.dataset.color;
+    }, 200);
 
     if (first === null) {
       first = box;
@@ -29,15 +48,14 @@ for (let i = 0; i < 12; i++) {
       second = box;
       lock = true;
 
-
       if (first.dataset.color === second.dataset.color) {
         first = null;
         second = null;
         lock = false;
       } else {
-        setTimeout(function () {
-          first.style.background = "";
-          second.style.background = "";
+        setTimeout(() => {
+          first.querySelector(".back").style.background = "";
+          second.querySelector(".back").style.background = "";
 
           first.classList.remove("open");
           second.classList.remove("open");
